47115fe84c7c40230e660cdeb542d9d991dc42d8
Olatunde Olatunde <olatunde.oladunni.dev@gmail.com>
2022-03-07 17:39:19 +0100

    Updates

--- src/utils/constants.js
+++
@@ -1,6 +0,0 @@
-export const PARAM_VIEW_MODE = 'view_mode';
-export const VIEW_MODE_WHISTLE = 'whistle';
-export const WHISTLE_SIGNUP_COMPLETE = 'whistle_signup_complete';
-export const SIGNUP_URL = 'https://signup.hive.io';
-export const SUBMIT_FORM_ID = 'submitStory';
-export const EVENT_OPERATION_BROADCAST = 'event_operation_broadcast';
